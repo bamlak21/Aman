6639862d9d7b26c75a1e76b8e1ffdf3c8e1bf572
bamlak21 <bamlakgezahegn27@gmail.com>
2026-01-13 13:58:11 +0300

    login page

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,99 @@
+import { SideImage } from "../components/SideImage";
+import { useForm } from "react-hook-form";
+import type { UserLogin } from "../types/auth";
+import { login } from "../api/auth";
+
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserLogin>();
+
+  const submit = async (data: UserLogin) => {
+    console.log(data);
+    login(data);
+  };
+
+  return (
+    <div className="flex h-dvh p-2">
+      {/* Place Holder image */}
+      <SideImage />
+      {/* Sign In Form */}
+      <div className="flex-1 h-full w-full flex flex-col items-center justify-center">
+        <div className="gap-5">
+          {/* Heading */}
+          <div className="flex flex-col items-center justify-center gap-2">
+            <h2 className="text-5xl font-semibold">Welcome Back</h2>
+            <p className="text-md text-gray-600">
+              Enter your email and password to access your account
+            </p>
+          </div>
+
+          <form className="mt-5" onSubmit={handleSubmit(submit)}>
+            <div className="flex flex-col justify-start gap-2 mb-4">
+              <label htmlFor="email" className="font-semibold tracking-wider">
+                Email
+              </label>
+              <input
+                className="p-2 bg-gray-200 rounded-lg outline-0 placeholder-gray-600"
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+                {...register("email", { required: "Email is required" })}
+              />
+              {errors.email && (
+                <p className="text-red-500">{errors.email.message}</p>
+              )}
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="password" className="font-semibold">
+                Password
+              </label>
+              <input
+                className="p-2 bg-gray-200 rounded-lg outline-0 placeholder-gray-600"
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+                {...register("password", { required: "Password is required" })}
+              />
+
+              {errors.password && (
+                <p className="text-red-500">{errors.password.message}</p>
+              )}
+            </div>
+            <div className="mt-2 flex justify-between">
+              <label htmlFor="remember">
+                <input
+                  type="checkbox"
+                  name="remember"
+                  id="remember"
+                  placeholder="Remember Me"
+                  className="mr-2"
+                />
+                Remember Me
+              </label>
+              <a href="#" className="text-[hsl(var(--accent))]">
+                Forgot password?
+              </a>
+            </div>
+            <button
+              className="mt-3 mx-auto bg-[hsl(var(--accent))] text-white w-full h-10 rounded-md text-lg font-semibold"
+              type="submit"
+            >
+              Login
+            </button>
+          </form>
+        </div>
+        <p className="fixed bottom-10 ">
+          Don't have an account?{" "}
+          <a href="/sign-up" className="text-[hsl(var(--accent))]">
+            Sign up
+          </a>{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
