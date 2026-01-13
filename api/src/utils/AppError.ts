c2c8bdc19fabccfffc0549bb4d7a043ef09c24a5
bamlak21 <bamlakgezahegn27@gmail.com>
2026-01-13 13:44:02 +0300

    created an AppError class extending from Error class to handle error with in the app

---
+++ api/src/utils/AppError.ts
@@ -0,0 +1,14 @@
+export class AppError extends Error {
+  public status: string;
+  public statusCode: number;
+  public isOperational: boolean;
+
+  constructor(statusCode: number, message: string) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "error" : "fail";
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
