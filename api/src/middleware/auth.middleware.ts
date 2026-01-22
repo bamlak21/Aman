64b080c97922767449d2e4fc5948d0ae7c6ce29b
bamlak21 <bamlakgezahegn27@gmail.com>
2026-01-22 12:32:45 +0300

    middleware to validate incoming requests authenticity

---
+++ api/src/middleware/auth.middleware.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Response } from "express";
+import { AuthReq } from "../types/auth";
+import { AppError } from "../utils/AppError";
+import { verifyToken } from "../utils/jwt";
+
+export const protect = (req: AuthReq, _res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return next(new AppError(401, "Token not Provided"));
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const payload = verifyToken(token);
+    req.user = payload;
+    next();
+  } catch (error) {
+    return next(new AppError(401, "Invalid Token"));
+  }
+};
