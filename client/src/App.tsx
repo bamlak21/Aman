8389e0a10de7454cb8c09b7c76b3dbb6fe3b2a85
bamlak21 <bamlakgezahegn27@gmail.com>
2026-01-27 17:01:37 +0300

    added react-router-dom and project font using inter font

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import Test from "./pages/Test";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Layout from "./layout/Layout";
+import { Routes, Route } from "react-router-dom";
+import Transactions from "./pages/Transactions";
+import CreateEscrow from "./pages/CreateEscrow";
 
 function App() {
   return (
@@ -9,6 +14,12 @@ function App() {
       <Routes>
         <Route path="/sign-in" element={<Login />} />
         <Route path="/sign-up" element={<Register />} />
+        <Route path="/" element={<Test />} />
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/create-escrow" element={<CreateEscrow />} />
+        </Route>
       </Routes>
     </>
   );
