8d681c3aedcb1443b96b904cf34acaa4994c8c79
bamlak21 <bamlakgezahegn27@gmail.com>
2026-02-02 14:18:48 +0300

    update for the user role

--- api/src/drizzle/schema/user.schema.ts
+++ api/src/drizzle/schema/user.schema.ts
@@ -1,12 +1,12 @@
 import { varchar, uuid, timestamp, pgTable, pgEnum } from "drizzle-orm/pg-core";
 
-export const userEnum = pgEnum("user_role", ["client", "freelancer"]);
+export const userEnum = pgEnum("user_role", ["payee", "payer"]);
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: varchar("name", { length: 255 }).notNull(),
   email: varchar("email", { length: 255 }).notNull().unique(),
   password: varchar("password").notNull(),
-  role: userEnum("user_role").default("client").notNull(),
-  createdAt: timestamp("created_at").defaultNow(),
+  role: userEnum("user_role").default("payee").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
 });
