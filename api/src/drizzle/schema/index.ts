1fbf447e2e5d5f780edc18c2e55618e7516702fb
bamlak21 <bamlakgezahegn27@gmail.com>
2026-02-04 16:58:26 +0300

    update

--- api/src/drizzle/schema/index.ts
+++ api/src/drizzle/schema/index.ts
@@ -1,4 +1,5 @@
 export * from "./user.schema";
 export * from "./escrow.schema";
-export * from "./escrowParties";
-export * from "./transactions";
+export * from "./parties.schema";
+export * from "./transactions.schema";
+export * from "./refreshToken.schema";
