65b6be43269aee7d88f255d519e5092db91de1d9
bamlak21 <bamlakgezahegn27@gmail.com>
2026-02-04 16:59:18 +0300

    added functions to generate and verify access tokens

--- api/src/utils/jwt.ts
+++ api/src/utils/jwt.ts
@@ -4,10 +4,23 @@ import { config } from "../config";
 import { AppError } from "./AppError";
 
 export const generateToken = (payload: JwtPayload) => {
-  return jwt.sign(payload, config.secret, { expiresIn: "24h" });
+  return jwt.sign(payload, config.secret, { expiresIn: "15m" });
+};
+
+export const generateRefreshToken = (payload: JwtPayload) => {
+  return jwt.sign(payload, config.secret, { expiresIn: "15d" });
 };
 
 export const verifyToken = (token: string) => {
+  try {
+    return jwt.verify(token, config.secret) as JwtPayload;
+  } catch (error) {
+    console.error("Invalid Token: ", error);
+    throw new AppError(409, "InValid Token");
+  }
+};
+
+export const verifyRefreshToken = (token: string) => {
   try {
     return jwt.verify(token, config.secret) as JwtPayload;
   } catch (error) {
